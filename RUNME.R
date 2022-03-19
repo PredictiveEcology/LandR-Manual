5bf4e38abef126809bcbc081ecdecdc4d5f9f782
CeresBarros <cbarros@mail.ubc.ca>
2022-03-18 21:04:57 -0700

    latex fixes:
    * download badges
    * normalizePath for figures
    * allow html
    * no underscore in .bib

--- RUNME.R
+++ RUNME.R
@@ -25,14 +25,25 @@ knitr::write_bib(c(
 ## collapse all chapter .bib files into one ------
 bibFiles <- c(list.files("modules", "references_", recursive = TRUE, full.names = TRUE),
               "citations/packages.bib",
-              "citations/references_LandRManual.bib")
+              "citations/referencesLandRManual.bib")
 bibdata <- lapply(bibFiles, readLines)
-write(unlist(bibdata), file = "citations/references_LandRManual.bib")
+write(unlist(bibdata), file = "citations/referencesLandRManual.bib")
 
 if (!file.exists("citations/ecology-letters.csl")) {
   dir.create("citations", showWarnings = FALSE)
   download.file("https://www.zotero.org/styles/ecology-letters?source=1", destfile = "citations/ecology-letters.csl")
 }
+
+## BADGE IMAGES --------------------------------------
+## add these to main figures/ folder so they can be used throughout module manuals when knitting to pdf
+dir.create("figures", showWarnings = FALSE)
+
+download.file(url = "https://img.shields.io/badge/Made%20with-Markdown-1f425f.png",
+              destfile = "figures/markdownBadge.png",
+              mode = 'wb')
+download.file(url = "https://img.shields.io/badge/Get%20help-Report%20issues-%3CCOLOR%3E.png",
+              destfile = "figures/genericBadge.png",
+              mode = 'wb')
 
 ## RMD PREP ------------------------------------------
 ## strip module.Rmd YAML headers -----
